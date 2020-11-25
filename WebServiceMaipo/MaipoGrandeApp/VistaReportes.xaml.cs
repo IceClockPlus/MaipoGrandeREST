e612080c9a9da1c1a1e6500dc209f15068041d25
Alexis <aravenapro98@gmail.com>
2020-11-25 17:24:25 -0300

    Implementacion Temporal de DevExpress
    Generacion de Reportes con ReportViewer

--- WebServiceMaipo/MaipoGrandeApp/VistaReportes.xaml.cs
+++ WebServiceMaipo/MaipoGrandeApp/VistaReportes.xaml.cs
@@ -31,8 +31,78 @@ namespace MaipoGrandeApp
         public VistaReportes(MenuPrincipal m)
         {
             InitializeComponent();
+            _reportViewer.Load += ReportViewer_Load;
             main = m;
         }
+
+
+        public List<DocumentoVenta> ObtenerDocumentosVenta()
+        {
+            try
+            {
+                List<DocumentoVenta> docs = new List<DocumentoVenta>();
+
+                RestClient client = new RestClient("http://localhost:54192/api");
+                RestRequest request = new RestRequest("/DocumentoVenta", Method.GET);
+                var response = client.Execute(request);
+                if (response.StatusCode == HttpStatusCode.OK)
+                {
+                    var documentos = JsonConvert.DeserializeObject<List<DocumentoVenta>>(response.Content);
+                    docs = documentos;
+                }
+                return docs;
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return new List<DocumentoVenta>();
+            }
+        }
+
+
+        private void ReportViewer_Load(object sender, EventArgs e)
+        {
+            
+            List<DocumentoVenta> list = new List<DocumentoVenta>();
+            //dataset = this.ObtenerDocumentosVenta();
+            list = this.ObtenerDocumentosVenta();
+
+            List<VentasReportes> dataset = new List<VentasReportes>();
+            foreach(var doc in list) {
+
+                VentasReportes vr = new VentasReportes();
+                vr.IdDocumento = doc.IdDocumento;
+                vr.FechaEmision = doc.FechaEmision;
+                vr.Pais = doc.Pedido.Pais;
+                vr.Cliente = doc.Pedido.Cliente.Nombre;
+                vr.EstadoDocumento = doc.EstadoDocumento.Descripcion;
+                vr.PrecioProducto = doc.PrecioProducto;
+                vr.PrecioTransporte = doc.PrecioTransporte;
+                vr.Impuesto = doc.Impuesto;
+                vr.Subtotal = doc.Subtotal;
+                vr.Total = doc.Total;
+                dataset.Add(vr);
+            }
+
+
+            _reportViewer.LocalReport.DataSources.Clear();
+            var rpds_model = new ReportDataSource() { Name = "VentasDS", Value = dataset };
+            _reportViewer.LocalReport.DataSources.Add(rpds_model);
+
+            this._reportViewer.LocalReport.ReportEmbeddedResource = "MaipoGrandeApp.ReporteVentas.rdlc";
+
+            _reportViewer.Refresh();
+            _reportViewer.RefreshReport();
+
+
+            //fill data into adventureWorksDataSet
+
+
+            
+        }
+
+
 
     }
 }
