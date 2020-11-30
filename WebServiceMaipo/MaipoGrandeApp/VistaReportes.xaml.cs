21fe934514b2dba209c9cd7cba9e36dbedae3f10
Alexis <aravenapro98@gmail.com>
2020-11-30 19:21:25 -0300

    Implementacion Parcial de SubInforme
    Se ha implementado el subinforme de ventas por cliente. Se requiere ajustes para impedir errores inesperados

--- WebServiceMaipo/MaipoGrandeApp/VistaReportes.xaml.cs
+++ WebServiceMaipo/MaipoGrandeApp/VistaReportes.xaml.cs
@@ -19,6 +19,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Microsoft.Reporting.WinForms;
+using System.IO;
 
 namespace MaipoGrandeApp
 {
@@ -27,76 +28,100 @@ namespace MaipoGrandeApp
     /// </summary>
     public partial class VistaReportes : Page
     {
+
+        private List<VentasReportes> ventas = new List<VentasReportes>();
+
         MenuPrincipal main;
         public VistaReportes(MenuPrincipal m)
         {
             InitializeComponent();
+            this.ObtenerDocumentosVenta();
             _reportViewer.Load += ReportViewer_Load;
             main = m;
         }
 
 
-        public List<DocumentoVenta> ObtenerDocumentosVenta()
+        /// <summary>
+        /// Metodo para obtener los datos de las ventas
+        /// </summary>
+        /// <returns></returns>
+        public void ObtenerDocumentosVenta()
         {
             try
             {
-                List<DocumentoVenta> docs = new List<DocumentoVenta>();
+                List<VentasReportes> docs = new List<VentasReportes>();
 
                 RestClient client = new RestClient("http://localhost:54192/api");
                 RestRequest request = new RestRequest("/DocumentoVenta", Method.GET);
                 var response = client.Execute(request);
+                //Se carga los datos de las ventas
                 if (response.StatusCode == HttpStatusCode.OK)
                 {
                     var documentos = JsonConvert.DeserializeObject<List<DocumentoVenta>>(response.Content);
-                    docs = documentos;
+                    foreach(DocumentoVenta doc in documentos)
+                    {
+                        VentasReportes vr = new VentasReportes();
+                        vr.IdDocumento = doc.IdDocumento;
+                        vr.FechaEmision = doc.FechaEmision;
+                        vr.Pais = doc.Pedido.Pais;
+                        vr.Cliente = doc.Pedido.Cliente.Nombre;
+                        vr.EstadoDocumento = doc.EstadoDocumento.Descripcion;
+                        vr.PrecioProducto = doc.PrecioProducto;
+                        vr.PrecioTransporte = doc.PrecioTransporte;
+                        vr.Impuesto = doc.Impuesto;
+                        vr.Subtotal = doc.Subtotal;
+                        vr.Total = doc.Total;
+                        ventas.Add(vr);
+                    }
+
                 }
-                return docs;
 
             }
             catch (Exception ex)
             {
                 Console.WriteLine(ex.Message);
-                return new List<DocumentoVenta>();
             }
         }
 
+        /// <summary>
+        /// Metodo para llenar el dataset del subinforme VentasCliente  
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void SubVentasClienteSubreportProcessing(object sender, SubreportProcessingEventArgs e)
+        {
+            if (ventas == null)
+                ObtenerDocumentosVenta();
+            e.DataSources.Add(new ReportDataSource("VentasClienteDS", ventas));
+
+        }
 
         private void ReportViewer_Load(object sender, EventArgs e)
         {
             
-            List<DocumentoVenta> list = new List<DocumentoVenta>();
-            //dataset = this.ObtenerDocumentosVenta();
-            list = this.ObtenerDocumentosVenta();
-
             List<VentasReportes> dataset = new List<VentasReportes>();
-            foreach(var doc in list) {
-
-                VentasReportes vr = new VentasReportes();
-                vr.IdDocumento = doc.IdDocumento;
-                vr.FechaEmision = doc.FechaEmision;
-                vr.Pais = doc.Pedido.Pais;
-                vr.Cliente = doc.Pedido.Cliente.Nombre;
-                vr.EstadoDocumento = doc.EstadoDocumento.Descripcion;
-                vr.PrecioProducto = doc.PrecioProducto;
-                vr.PrecioTransporte = doc.PrecioTransporte;
-                vr.Impuesto = doc.Impuesto;
-                vr.Subtotal = doc.Subtotal;
-                vr.Total = doc.Total;
-                dataset.Add(vr);
-            }
-
+            //dataset = this.ObtenerDocumentosVenta();
+            dataset = ventas;    
 
             _reportViewer.LocalReport.DataSources.Clear();
-            var rpds_model = new ReportDataSource() { Name = "VentasClienteDS", Value = dataset };
+            var rpds_model = new ReportDataSource() { Name = "DataVentas", Value = ventas };
             _reportViewer.LocalReport.DataSources.Add(rpds_model);
 
-            this._reportViewer.LocalReport.ReportEmbeddedResource = "MaipoGrandeApp.ReporteVentasCliente.rdlc";
+            //Obtener la url de la solucion de escritorio y concatenar con la ubicacion del reporte 
+            string startupPath = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName+ "\\Reportes\\Reporte.rdlc";
+
+            //Establecer ubicacion del reporte principal
+            this._reportViewer.LocalReport.ReportPath = startupPath;
+
+            //Llamar evento para llenar los datos de los subinformes
+            this._reportViewer.LocalReport.SubreportProcessing += new SubreportProcessingEventHandler(SubVentasClienteSubreportProcessing);
+
+            //Llamar eventa para llenar los datos del grafico del subinforme VentaClienteGrafico
 
             _reportViewer.Refresh();
             _reportViewer.RefreshReport();
 
 
-            //fill data into adventureWorksDataSet
 
 
             
