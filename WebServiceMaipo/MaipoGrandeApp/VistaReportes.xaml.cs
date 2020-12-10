6d857453c020778183ed56fa816d6f2ce3247b45
Alexis <aravenapro98@gmail.com>
2020-12-10 02:11:11 -0300

    Reporte de Satisfaccion

--- WebServiceMaipo/MaipoGrandeApp/VistaReportes.xaml.cs
+++ WebServiceMaipo/MaipoGrandeApp/VistaReportes.xaml.cs
@@ -36,10 +36,18 @@ namespace MaipoGrandeApp
         public VistaReportes(MenuPrincipal m)
         {
             InitializeComponent();
+            CargarComboReportes();
             _reportViewer.Load += ReportViewer_Load;
             main = m;
         }
 
+        public void CargarComboReportes()
+        {
+            IEnumerable<string> reportes = Enumerable.Empty<string>();
+            reportes = new string[] { "Ventas Cliente", "Encuestas"};
+            CbxReportes.ItemsSource = reportes;
+
+        }
 
         /// <summary>
         /// Metodo para obtener los datos de las ventas
@@ -86,6 +94,31 @@ namespace MaipoGrandeApp
         }
 
         /// <summary>
+        /// Metodo para obtener todas las encuestas de satisfaccion
+        /// </summary>
+        /// <returns></returns>
+        public List<Encuesta> ObtenerEncuestas()
+        {
+            try
+            {
+
+                RestClient client = new RestClient("http://localhost:54192/api");
+                RestRequest request = new RestRequest("/Encuesta", Method.GET);
+                var response = client.Execute(request);
+
+                List<Encuesta> list = JsonConvert.DeserializeObject<List<Encuesta>>(response.Content);
+                return list;
+
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return new List<Encuesta>();
+            }
+        }
+
+
+        /// <summary>
         /// Metodo para llenar el dataset del subinforme VentasCliente  
         /// </summary>
         /// <param name="sender"></param>
@@ -99,6 +132,20 @@ namespace MaipoGrandeApp
             e.DataSources.Add(new ReportDataSource("HistoricoVentaClienteDS", ventas));
         }
 
+        /// <summary>
+        /// Evento para llenar el dataset para los subinformes de Encuestas de Satisfaccion
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void ReporteEncuestaSubreportProcessing(object sender, SubreportProcessingEventArgs e)
+        {
+            //Llamar al metodo para obtener las encuestas del servicio REST
+            List<Encuesta> dataset = this.ObtenerEncuestas();
+            //Asignar los datos al datasource de los subinformes
+            e.DataSources.Add(new ReportDataSource("EncuestaDS", dataset));
+            e.DataSources.Add(new ReportDataSource("EncuestaGraficoDS", dataset));
+
+        }
 
 
         private void ReportViewer_Load(object sender, EventArgs e)
@@ -108,23 +155,54 @@ namespace MaipoGrandeApp
             ObtenerDocumentosVenta();
             dataset = ventas;    
 
-            _reportViewer.LocalReport.DataSources.Clear();
+            //_reportViewer.LocalReport.DataSources.Clear();
             var rpds_model = new ReportDataSource() { Name = "DataVentas", Value = ventas };
             _reportViewer.LocalReport.DataSources.Add(rpds_model);
 
             //Obtener la url de la solucion de escritorio y concatenar con la ubicacion del reporte 
-            string startupPath = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName+ "\\Reportes\\VentasCliente\\ReporteVenta.rdlc";
+            //string startupPath = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName+ "\\Reportes\\VentasCliente\\ReporteVenta.rdlc";
 
             //Establecer ubicacion del reporte principal
-            this._reportViewer.LocalReport.ReportPath = startupPath;
+            //this._reportViewer.LocalReport.ReportPath = startupPath;
 
             //Llamar evento para llenar los datos de los subinformes
-            this._reportViewer.LocalReport.SubreportProcessing += new SubreportProcessingEventHandler(SubVentasClienteSubreportProcessing);
+            
 
+        }
+
+
+        private void CbxReportes_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string reporte = CbxReportes.SelectedItem.ToString();
+            //Establecer ubicacion del archivo del proyecto MaipoGrandeApp
+            string startupPath = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName;
+
+            
+            switch (reporte)
+            {
+                case "Ventas Cliente":
+                    startupPath += "\\Reportes\\VentasCliente\\ReporteVenta.rdlc";
+                    this._reportViewer.LocalReport.ReportPath = startupPath;
+                    this._reportViewer.LocalReport.SubreportProcessing += new SubreportProcessingEventHandler(SubVentasClienteSubreportProcessing);
+                    break;
+                case "Encuestas":
+                    startupPath += "\\Reportes\\EncuestaSatisfaccion\\ReporteEncuesta.rdlc";
+                    this._reportViewer.LocalReport.ReportPath = startupPath;
+                    this._reportViewer.LocalReport.SubreportProcessing += new SubreportProcessingEventHandler(ReporteEncuestaSubreportProcessing);
+                    break;
+                default:
+                    break;
+            }
+
+
+            //Obtener la url de la solucion de escritorio y concatenar con la ubicacion del reporte 
+            //string startupPath = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName + "\\Reportes\\VentasCliente\\ReporteVenta.rdlc";
+
+            //Establecer ubicacion del reporte principal
+            //this._reportViewer.LocalReport.ReportPath = startupPath;
 
             _reportViewer.Refresh();
             _reportViewer.RefreshReport();
-
         }
     }
 }
